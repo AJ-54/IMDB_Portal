8931a72067eb092d6f1c165f96632f44a08e858d
Ayush Jain <ayushjai@iitg.ac.in>
2018-12-03 15:23:39 +0530

    Update imdb.py

--- imdb.py
+++ imdb.py
@@ -20,25 +20,25 @@ def info_movie():
 	t = name.replace(' ','%20')	
 	
 	# The url is stored inside the variable and python can open the url using request module. We need a unique api_key to extract data from domain.	
-    url = 'https://api.themoviedb.org/3/search/movie?api_key=ffb07b773769d55c36ccd83845385205&language=en-US&query='+str(t)+'&page=1&include_adult=false'
-    response = requests.get(url)
+    	url = 'https://api.themoviedb.org/3/search/movie?api_key=ffb07b773769d55c36ccd83845385205&language=en-US&query='+str(t)+'&page=1&include_adult=false'
+    	response = requests.get(url)
 	
 	# The response cannot be printed directly as it contains data in dictionary form which is json type.  Python has inbuit library json which takes care of 
 	# data files being exchanged between web server and browser.		
-    u = json.loads(response.text)
+    	u = json.loads(response.text)
 	
 	
 	# The above variable u stores the result of your search and next two lines extracts the topmost movie and its unique id.
 	results  = u['results']
-    id = results[0]['id']
+    	id = results[0]['id']
 	
 	# The below url2 variable stores the link which contains complete information of our movie.	
-    url2 = 'https://api.themoviedb.org/3/movie/'+str(id)+'?api_key=ffb07b773769d55c36ccd83845385205&language=en-US'
-    response = requests.get(url2)
-    w = json.loads(response.text)
+    	url2 = 'https://api.themoviedb.org/3/movie/'+str(id)+'?api_key=ffb07b773769d55c36ccd83845385205&language=en-US'
+    	response = requests.get(url2)
+    	w = json.loads(response.text)
 	
 	# w now contains all our information in list with dictionary elements . We can extract our information by referring to the keys.
-    try:
+   	try:
 	
 		# Variables point towards the keys and have our desired results.
 		title = w['title']
@@ -81,46 +81,46 @@ def info_movie():
 		status.write ("\n\t GENRE       : \t\t"+genre[0]['name'])
 		status.write ("\n\t PLOT        : \t\t"+plot)
 
-    except KeyError:
-        print "\nNo such movie titled '"+name+"' found!\n"
-        status.write ("\nNo such movie titled '"+name+"' found!\n")
+    	except KeyError:
+        	print "\nNo such movie titled '"+name+"' found!\n"
+        	status.write ("\nNo such movie titled '"+name+"' found!\n")
     
     
 def top_movies():
-    x = input("\nEnter n, to display Top 'n' movies: " )
-    url = 'http://www.imdb.com/chart/top'
-    response = requests.get(url)
-    html = response.text
+    	x = input("\nEnter n, to display Top 'n' movies: " )
+    	url = 'http://www.imdb.com/chart/top'
+    	response = requests.get(url)
+    	html = response.text
 	
 	# Stores the complete webpage html code
-    soup = bs4.BeautifulSoup(html,"lxml")			
-    rows = soup.select('.lister-list tr')				
-    print ("\n"+"----------------------------------TOP "+str(x)+" MOVIES ACCORDING TO IMDB RATINGS---------------------------------"+"\n\n")
-    print (" \t   TITLE\t\t\t\t\t\t\t\t\t\t   IMDB RATING\n\n")
-    status.write ("\n"+"---------------------------TOP "+str(x)+" MOVIES ACCORDING TO IMDB RATINGS-----------------------------"+"\n\n")
-    status.write (" \t   TITLE\t\t\t\t\t\t\t\t\t\t   IMDB RATING\n\n")
+   	soup = bs4.BeautifulSoup(html,"lxml")			
+    	rows = soup.select('.lister-list tr')				
+    	print ("\n"+"----------------------------------TOP "+str(x)+" MOVIES ACCORDING TO IMDB RATINGS---------------------------------"+"\n\n")
+    	print (" \t   TITLE\t\t\t\t\t\t\t\t\t\t   IMDB RATING\n\n")
+    	status.write ("\n"+"---------------------------TOP "+str(x)+" MOVIES ACCORDING TO IMDB RATINGS-----------------------------"+"\n\n")
+    	status.write (" \t   TITLE\t\t\t\t\t\t\t\t\t\t   IMDB RATING\n\n")
     
-    for row in range(0,x):
-        tdata=rows[row].select('td')
+    	for row in range(0,x):
+        	tdata=rows[row].select('td')
 		# Extracts name of movie
-        name=tdata[1].get_text(' ',strip=True)
+        	name=tdata[1].get_text(' ',strip=True)
 		# Extracts rating of movie
-        rating=tdata[2].get_text(' ',strip=True)			 					
-        ans=("\n "+name.ljust(75,' ')+"\t\t\t\t"+rating+"\n")
-        ans=ans.encode('ascii','ignore')
-        print ans
+        	rating=tdata[2].get_text(' ',strip=True)			 					
+        	ans=("\n "+name.ljust(75,' ')+"\t\t\t\t"+rating+"\n")
+        	ans=ans.encode('ascii','ignore')
+        	print ans
 		# Appends data in file info.txt 
-        status.write (ans)											 					
+        	status.write (ans)											 					
         
         
 def folder():
-    path = raw_input("\n\nEnter the complete path of the directory where your movies are present: ")
-    dirs = os.listdir(path)
-    print "Showing results for the path: "+path+"\n"
-    status.write ('Showing results for the path: '+path+'\n')
+    	path = raw_input("\n\nEnter the complete path of the directory where your movies are present: ")
+    	dirs = os.listdir(path)
+    	print "Showing results for the path: "+path+"\n"
+    	status.write ('Showing results for the path: '+path+'\n')
 	
 	# The process is similar to the one encountered in method info_movie.   
-    for i in range(len(dirs)):
+    	for i in range(len(dirs)):
 		x = dirs[i]
 		t = x.replace(' ','%20')
 		url = 'https://api.themoviedb.org/3/search/movie?api_key=ffb07b773769d55c36ccd83845385205&language=en-US&query='+str(t)+'&page=1&include_adult=false'
@@ -158,27 +158,27 @@ def folder():
 
 
 def driver():
-    print "\n\n\t\t\t\t\t----------------IMDB PORTAL--------------------"
-    status.write("\\\n\n\t\t\t\t\t---------------------IMDB PORTAL----------------------")
-    choice=input('Enter your choice:\n\n1) Search movie information by title\n2) Show top rated movies\n3) Rename folder with IMDB rating and year of release added to it\n\nInput: ')
+    	print "\n\n\t\t\t\t\t----------------IMDB PORTAL--------------------"
+    	status.write("\\\n\n\t\t\t\t\t---------------------IMDB PORTAL----------------------")
+    	choice=input('Enter your choice:\n\n1) Search movie information by title\n2) Show top rated movies\n3) Rename folder with IMDB rating and year of release added to it\n\nInput: ')
     
-    if(choice == 1):
-        info_movie()
-    elif(choice == 2):
-        top_movies()
-    else:
-            folder()
+    	if(choice == 1):
+        	info_movie()
+    	elif(choice == 2):
+        	top_movies()
+    	else:
+            	folder()
             
         
 driver()
 while (1>0) :
-    repeat = raw_input("\n\nDo you want to try again?(type 'Yes'/'Y'/'y' or else press anything) ")
-    if (repeat == 'Yes') or (repeat == 'Y') or (repeat == 'y'):
-        os.system('clear')
-        driver()
-    else:
-        print "\nThank you for using!"
-        status.write("\nThank you for using!")
-        break
+    	repeat = raw_input("\n\nDo you want to try again?(type 'Yes'/'Y'/'y' or else press anything) ")
+	if (repeat == 'Yes') or (repeat == 'Y') or (repeat == 'y'):
+        	os.system('clear')
+        	driver()
+    	else:
+        	print "\nThank you for using!"
+        	status.write("\nThank you for using!")
+        	break
 # Closes the text file 
 status.close()
